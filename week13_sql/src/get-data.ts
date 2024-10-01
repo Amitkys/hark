ed0ef8aeb1205622b08750e59bd602b806bd0936
KYs <amitkys59@gmail.com>
2024-10-02 00:18:35 +0530

    added a new function for fetching user information

--- week13_sql/src/get-data.ts
+++ week13_sql/src/get-data.ts
@@ -2,12 +2,34 @@ import { getClient } from "./utils";
 
 async function getUserData() {
     const client = await getClient();
+
     const getUserQuery = "SELECT * FROM users";
+
     const userRes = await client.query(getUserQuery);
+
     console.log('all users');
     for(let user of userRes.rows) {
         console.log(`ID: ${user.id}, EMAIL: ${user.email}`);
 
     }
 }
-getUserData();
+
+async function getUserFromEmail(email: string) {
+    const client = await getClient();
+    
+    const selectUserText = 'SELECT * FROM users WHERE email = $1';
+    const userRes = await client.query(selectUserText, [email]);
+
+    console.log('single user details (by email): ');
+
+    for (let user of userRes.rows) {
+        console.log(`ID ${user.id}, EMAIL: ${user.email}`);
+    }
+
+}
+
+async function 
+
+
+// getUserData();
+getUserFromEmail('amit@gmail.com');
